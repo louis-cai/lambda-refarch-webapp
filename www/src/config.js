9bc30736b1f7a854cfc210814c933b4b842f22e5
蔡路 <15198000>
2023-04-19 14:18:56 +0800

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "AWS_USER_POOLS_WEB_CLIENT_ID",     // CognitoClientID
+  "api_base_url": "API_BASE_URL",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "COGNITO_HOSTED_DOMAIN",                   // CognitoDomainName
+  "redirect_url": "REDIRECT_URL"                                      // AmplifyURL
 };
 
 export default config;
